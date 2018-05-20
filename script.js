5dbfb89075f9bab7ccdf800c17bd5578e9dd9c4a
yurypopeko <yurypopeko@gmail.com>
2018-05-20 10:29:46 +0300

    modal tour

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', function(e) {
+	e = e || window.event;
+	const target = e.target;
+
+	if (target.closest('button.close')) {
+		target.closest('button.close').parentElement.classList.remove('open');
+	} else if (target.closest('[class*=modal-]')) {
+		const modalClass = 'div.modal-' + target.closest('[class*=modal-]').className.slice(6);
+		const modalWindow = document.querySelector(modalClass);
+		modalWindow.classList.add('open')
+	}
+})
